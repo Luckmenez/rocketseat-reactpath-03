1b43d97c5292622ba38fbf9ac2e8ac533bd74645
Lucas Menezes <lucasmenezes@MacBook-Air-de-Lucas.local>
2023-12-04 22:23:57 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Transactions } from './pages/Transactions'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Transactions />
+    </ThemeProvider>
+  )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+import logoImage from '../../assets/Logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImage} alt="" />
+
+        <NewTransactionButton>Nova transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
